1d19fc699a86a379518eff6849d2f019f5c857c6
jozsef <pallagijoe@gmail.com>
2025-10-04 16:16:38 +0700

    BE and FE integration

--- ats-ui/src/app/app.component.ts
+++ ats-ui/src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooService } from './foo.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ats-ui';
+
+  private fooService$: FooService = inject(FooService);
+  hello$ = this.fooService$.getHello();
+
 }
---
+++ ats-ui/src/app/foo.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FooService {
+  private httpClient: HttpClient = inject(HttpClient);
+
+  getHello() {
+    return this.httpClient.get<string>('http://localhost:3000');
+  }
+}
