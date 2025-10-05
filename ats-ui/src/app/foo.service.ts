e8a2c9dbea3d7aef54213d0e150f4266392ddeca
jozsef <pallagijoe@gmail.com>
2025-10-05 08:52:27 +0700

    fix: update API endpoint in FooService for correct URL

--- ats-ui/src/app/foo.service.ts
+++ ats-ui/src/app/foo.service.ts
@@ -8,6 +8,6 @@ export class FooService {
   private httpClient: HttpClient = inject(HttpClient);
 
   getHello() {
-    return this.httpClient.get<string>('https://ats-web-1041765018913.europe-central2.run.app/');
+    return this.httpClient.get<string>('https://ats-api-1041765018913.europe-central2.run.app/');
   }
 }
